bc886fd0e89f6205fcc91e7181fedf825634ade1
Daniil Shcherbakov <Charel@bk.ru>
2024-12-12 22:32:38 +0300

    Test DataPickerDialog

--- app/src/main/java/ru/nvgsoft/workaccounting/MainActivity.kt
+++ app/src/main/java/ru/nvgsoft/workaccounting/MainActivity.kt
@@ -1,15 +1,43 @@
 package ru.nvgsoft.workaccounting
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.nvgsoft.workaccounting.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        binding.btnGetData.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        // Получаем текущую дату
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Создаем и показываем диалог выбора даты
+        val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+            // Обновляем текстовое поле с выбранной датой
+            val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear" // Месяцы начинаются с 0
+            binding.tvData.text = selectedDate
+        }, year, month, day)
+
+        datePickerDialog.show()
     }
 }
