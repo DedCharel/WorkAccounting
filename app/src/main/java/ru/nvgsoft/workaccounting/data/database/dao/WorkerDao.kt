8ccc6676fcf6dcec898c3445107d6410439141bb
Daniil Shcherbakov <Charel@bk.ru>
2024-12-18 12:42:06 +0300

    WorkerDao

--- app/src/main/java/ru/nvgsoft/workaccounting/data/database/dao/WorkerDao.kt
+++ app/src/main/java/ru/nvgsoft/workaccounting/data/database/dao/WorkerDao.kt
@@ -1,10 +1,26 @@
 package ru.nvgsoft.workaccounting.data.database.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.nvgsoft.workaccounting.data.database.model.WorkItemDbModel
+import ru.nvgsoft.workaccounting.data.database.model.WorkerDbModel
 
 @Dao
 interface WorkerDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addWorker(worker: WorkerDbModel)
 
+    @Query("DELETE FROM workers WHERE id=:workerId")
+    fun deleteWorker(workerId: Int)
+
+    @Query("SELECT * FROM workers WHERE id=:workerId LIMIT 1")
+    fun getWorker(workerId: Int): WorkerDbModel
+
+    @Query("SELECT * FROM  workers")
+    fun getWorkerList(): LiveData<List<WorkerDbModel>>
 
 }
