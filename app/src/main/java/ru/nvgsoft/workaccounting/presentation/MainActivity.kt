0bbb51ea2da596bfc976d13799986bb69b11785c
Daniil Shcherbakov <Charel@bk.ru>
2025-01-03 21:06:41 +0300

    Presentation layer. Bottom navigation

--- app/src/main/java/ru/nvgsoft/workaccounting/presentation/MainActivity.kt
+++ app/src/main/java/ru/nvgsoft/workaccounting/presentation/MainActivity.kt
@@ -3,6 +3,12 @@ package ru.nvgsoft.workaccounting.presentation
 import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.nvgsoft.workaccounting.R
 import ru.nvgsoft.workaccounting.databinding.ActivityMainBinding
 import java.util.Calendar
 
@@ -16,25 +22,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.btnGetData.setOnClickListener {
-            showDatePickerDialog()
-        }
+        val navView: BottomNavigationView = binding.navView
+
+        val navController =findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_works, R.id.navigation_directories, R.id.navigation_settings
+            )
+        )
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
 
-    private fun showDatePickerDialog() {
-        // Получаем текущую дату
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        // Создаем и показываем диалог выбора даты
-        val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
-            // Обновляем текстовое поле с выбранной датой
-            val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear" // Месяцы начинаются с 0
-            binding.tvData.text = selectedDate
-        }, year, month, day)
-
-        datePickerDialog.show()
-    }
+//    private fun showDatePickerDialog() {
+//        // Получаем текущую дату
+//        val calendar = Calendar.getInstance()
+//        val year = calendar.get(Calendar.YEAR)
+//        val month = calendar.get(Calendar.MONTH)
+//        val day = calendar.get(Calendar.DAY_OF_MONTH)
+//
+//        // Создаем и показываем диалог выбора даты
+//        val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+//            // Обновляем текстовое поле с выбранной датой
+//            val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear" // Месяцы начинаются с 0
+//            binding.tvData.text = selectedDate
+//        }, year, month, day)
+//
+//        datePickerDialog.show()
+//    }
 }
---
+++ app/src/main/java/ru/nvgsoft/workaccounting/presentation/ui/settings/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package ru.nvgsoft.workaccounting.presentation.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.nvgsoft.workaccounting.R
+
+
+
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+
+}
